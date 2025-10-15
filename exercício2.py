ff47fe26d63a7d1efcecf6b3298f80f4e88dd208
José Alfaia <Jose.Alfaia.T0129865@edu.atec.pt>
2025-10-15 14:43:33 +0100

    add exercício2

--- exercício2.py
+++ exercício2.py
@@ -12,9 +12,10 @@ elif num1 > num3 and num3 > num2:
     print(f"{num1} é o maior e o menor é o {num2}") 
 elif num2 > num3 and num3 > num1:
     print(f"{num2} é o maoir e o menor é o {num1}")
-elif num2 > num1 and num1 > num1 > num3 :
+elif num2 > num1 and  num1 > num3 :
     print(f"{num2} é o maoir e o menor é o {num3}")
 elif num3 > num2 and num2 > num1:
     print(f"{num3} é o maoir e o menor é o {num1}")    
 elif num3 > num1 and num1 > num2:
     print(f"{num3} é o maior e o menor é  o {num2}")
+   
